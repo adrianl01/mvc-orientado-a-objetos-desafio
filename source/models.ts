097164101290a2e87d5025611dae4c9c48167f6b
Adrian Leiva <gustavo.adrian.leiva879@gmail.com>
2023-01-24 18:55:09 -0300

    el desastre

--- source/models.ts
+++ source/models.ts
@@ -1,7 +1,37 @@
+import * as jsonfile from "jsonfile";
+
 class Contact {
   id: number = 0;
   name: string = "";
 }
 
-class ContactsCollection {}
+class ContactsCollection {
+  data: Contact[] = [];
+  load() {
+    const json = jsonfile.readFileSync(__dirname + "/contacts.json");
+    this.data = json;
+    console.log(this.data);
+  }
+
+  // getAll() {
+  //   return this.data;
+  // }
+
+  // addOne(contact: Contact) {
+  //   this.data.push(contact);
+  // }
+
+  // save() {
+  //   jsonfile.writeFileSync("./contacts.json", this.data);
+  // }
+
+  getOneById(id) {
+    const encontrado = this.data.find((item) => {
+      if (item.id == id) {
+        return true;
+      }
+    });
+    return encontrado;
+  }
+}
 export { ContactsCollection };
